c611df79753b5a341a091a1d3bccd8ed9c2bca52
Reynaldo Jimenez <rjimenez@students.mchenry.edu>
2024-11-21 01:45:49 -0600

    Add files via upload

--- AFtree.py
+++ AFtree.py
@@ -1,5 +1,5 @@
 #Animal Forest Dialogue
-
+import random
 
 class DialogueTree():
     def __init__(self):
@@ -8,38 +8,50 @@ class DialogueTree():
         self.savestates[choice] = outcome
 
 class Npc:
-    def __init__(self):
-        pass
-
-class Squirrel(Npc):
     def __init__(self, cutesy, tricksy, sneaksy, meany):
-        super().__init__()
         self.cutesy = cutesy
         self.tricksy = tricksy
         self.sneaksy = sneaksy
         self.meany = meany
+        pass
+
+    def cutesy_dice_roll(self):
+        roll = [random.randint(1,6) for x in range(self.cutesy)]
+        return roll
+    def sneaksy_dice_roll(self):
+        roll = [random.randint(1,6) for x in range(self.sneaksy)]
+        return roll
+    def meany_dice_roll(self):
+        roll = [random.randint(1,6) for x in range(self.meany)]    
+        return roll
+    
+class Squirrel(Npc):
+    def __init__(self,cutesy, tricksy, sneaksy, meany):
+        super().__init__(cutesy, tricksy, sneaksy, meany)
+        self.dialogue = {
+            'intro3': 'Squirrel: "But...but...oh okay.." *sniff* *sniff*',
+            'intro4': 'Squirrel: "No! NO! I eat it all now!" ***SQUIRREL RAGE***' 
+        }
         pass
     def introdiag(self):
         return '\nSquirrel: "Is that...is that honey...?"'
+    def introdiag2(self):
+        return 'Squirrel: "Deal but i get my half now!"'
+    
+
 
 class Opossum(Npc):
     def __init__(self, cutesy, tricksy, sneaksy, meany):
-        super().__init__()
-        self.cutesy = cutesy
-        self.tricksy = tricksy
-        self.sneaksy = sneaksy
-        self.meany = meany
+        super().__init__(cutesy, tricksy, sneaksy, meany)
+
         pass
     def introdiag(self):
         return '\nOPOSSUM: "So you\'re really going to go into town?"'
 
 class Raccoon(Npc):
     def __init__(self, cutesy, tricksy, sneaksy, meany):
-        super().__init__()
-        self.cutesy = cutesy
-        self.tricksy = tricksy
-        self.sneaksy = sneaksy
-        self.meany = meany
+        super().__init__(cutesy, tricksy, sneaksy, meany)
+
         pass
 
     def introdiag(self):
@@ -54,25 +66,56 @@ If not, then its just me. But I\'ll be alone with all this honey"\n')
         
         print('2) Raccoon: "There\'s no honey...what are you talking about?"\n')
 
-        print('3) Raccoon: "I will give you the entire thing if you come with us."\n')
+        print('3) Raccoon: "It\'s for me, but i will let you sniff it along the way."\n')
         
         introdiag_choice_made = int(input("Choose 1, 2, or 3: "))
         if introdiag_choice_made == 1:
-            outcome = 'Squirrel: "Maybe, I still have things to bury"'
-            print(outcome)
+            
+            roll = raccoon1.cutesy_dice_roll()
+            success = sum(1 for die in roll if die >= squirrel1.cutesy)
+            if success >= 2:
+                success = squirrel1.introdiag2()  #'Squirrel: "Deal but i get my half now!"'
+                print(raccoon1.cutesy_dice_roll())
+                print(success)
+                success = 'true'
+                dialogue.add_savestate(introdiag_choice_made, success)
+            else:  
+                failure = 'Squirrel: "Maybe, I still have things to bury"'
+                print(failure)
+                failure = 'false'
+                dialogue.add_savestate(introdiag_choice_made, failure)    
         elif introdiag_choice_made == 2:
-            outcome = 'Squirrel: "I see it. I seet it. Its honey in that leaf!"\
+            roll = raccoon1.sneaksy_dice_roll()            
+            success = sum(1 for die in roll if die >= squirrel1.sneaksy)
+            if success >= 2:
+                success = 'Squirrel: "I have to help bury more things today anyways."'
+                print(success)
+                success = 'success'
+                dialogue.add_savestate(introdiag_choice_made, success)
+            else:
+                failure = 'Squirrel: "I see it. I seet it. Its honey in that leaf!"\
 ***Eyes turn red SQUIRREL RAGE***'
-            print(outcome)
+                print(failure)
+                failure = 'false'
+                dialogue.add_savestate(introdiag_choice_made, failure)
         else:
-            print("NEED CODE FOR 'ROLLING'")
-            print(outcome)
-        dialogue.add_savestate(introdiag_choice_made, outcome)
+            roll = raccoon1.meany_dice_roll()            
+            success = sum(1 for die in roll if die >= squirrel1.meany)
+            if success >= 2:
+                success = squirrel1.dialogue['intro3']
+                print(success)
+                success = 'success'
+                dialogue.add_savestate(introdiag_choice_made, success)
+            else:
+                failure = squirrel1.dialogue['intro4']
+                print(failure)
+                failure = 'false'
+                dialogue.add_savestate(introdiag_choice_made, failure)
 
 dialogue = DialogueTree()
 opossum1 = Opossum(3, 2, 4, 4)
 raccoon1 = Raccoon(4, 5, 3, 2)
-squirrel1 = Squirrel(1, 3, 2, 5)
+squirrel1 = Squirrel(2, 3, 2, 5)
 
 def introdialogue():
 
@@ -84,3 +127,4 @@ def introdialogue():
 
 introdialogue()
 print(dialogue.savestates)
+
